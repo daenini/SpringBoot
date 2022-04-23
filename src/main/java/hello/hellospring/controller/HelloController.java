93718b301c31023230263412595491721b3844e0
daeni <eogus8292@naver.com>
2022-04-23 23:53:30 +0900

    2022.04.23 스프링 입문  - 스프링 웹 개발 기초 3 다음 회원 관리 예제 1부터 시작

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -18,5 +19,29 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model){
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody //http프로토콜 body에 직접 데이터를 넣어준다.
+    public String helloString(@RequestParam("name") String name){
+        return "hello"+name;
+    } //예제
+
+    //json 방식 (최근에 많이 씀)
+    @GetMapping("hello_api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello{
+        private  String name;
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
