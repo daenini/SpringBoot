c2abc924789352a3ad6f7d87c8473509b7d1a14b
daeni <eogus8292@naver.com>
2022-04-26 22:29:22 +0900

    2022.04.26 스프링 입문 - 회원 관리 예제 다음 스프링 DB 접근 기술

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 // 스프링 컨테이너가 컨트롤러 에노테이션이 있으면 객체 생성해서 스프링이 관리함 (스르링 빈이 관리됨)
@@ -15,5 +21,25 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
 
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return  "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list (Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+        return "members/memberList";
+    }
 }
