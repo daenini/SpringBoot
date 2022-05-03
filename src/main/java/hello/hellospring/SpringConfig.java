2c386afd3b4f144500e8ed89d739a1f6f6e77bbf
daeni <eogus8292@naver.com>
2022-05-03 21:42:48 +0900

    2022.05.03 JPA

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,22 +1,33 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcTempletMemberRepository;
+import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
+
+    private EntityManager em;
     private DataSource dataSource;
     @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(DataSource dataSource, EntityManager em) {
+        this.dataSource = dataSource;;
+        this.em = em;
     }
 
+    /*
+        private DataSource dataSource;
+        @Autowired
+        public SpringConfig(DataSource dataSource) {
+            this.dataSource = dataSource;
+        }
+        */
     @Bean
     public MemberService memberService(){
         return  new MemberService(memberRepository());
@@ -26,6 +37,7 @@ public class SpringConfig {
     public MemberRepository memberRepository(){
         // return new MemoryMemberRepository();
        //  return new JdbcMemberRepository(dataSource);
-        return  new JdbcTempletMemberRepository(dataSource);
+        //return  new JdbcTempletMemberRepository(dataSource);
+        return  new JpaMemberRepository(em);
     }
 }
